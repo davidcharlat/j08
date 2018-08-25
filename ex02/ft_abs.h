c3dc549256d7130f275e7dd1f175e19d9afba3c9
Thomas Charlat <thomas.charlat@gmail.com>
2018-08-25 19:43:40 +0200

    Update ft_abs.h

--- ex02/ft_abs.h
+++ ex02/ft_abs.h
@@ -1,12 +1,6 @@
 #ifndef __FT_ABS_H__
 #define __FT_ABS_H__
 
-int	ft_abs(int x)
-{
-	if (x < 0)
-		return (-x);
-	return (x);
-}
+#define	ABS(Value)	((Value < 0) ? (-Value) : (Value))
 
-# define ABS(value) ft_abs(value)
 #endif
